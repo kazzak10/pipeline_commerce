8a6ca3e7522dbd0439020a238fa565fe46a7e1dd
Zakaria Houari <zakaria.pro24@gmail.com>
2026-09-24 13:10:28 +0200

    Pipeline Spark Structured Streaming fonctionnel : Kafka vers PostgreSQL (silver.commandes_live)

--- spark_jobs/streaming_job.py
+++ spark_jobs/streaming_job.py
@@ -1,10 +1,13 @@
+import os
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType, StructField, StringType, FloatType, IntegerType
-from pyspark.sql.functions import from_json, col
+from pyspark.sql.functions import from_json, col, to_timestamp
 
 spark = SparkSession.builder.appName("spark_job").getOrCreate()
 KAFKA_BOOTSTRAP_SERVERS = "kafka:9092"
 KAFKA_TOPIC = "commandes"
+
+
 #on a que 1 seul broker 1 seul topic et 1 seule partition donc ca dans la doc 
 df = spark \
   .readStream \
@@ -13,6 +16,7 @@ df = spark \
   .option("subscribe", KAFKA_TOPIC) \
   .load()
 df_decoded = df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
+
 
 SCHEMA = StructType([
     StructField("commande_id", StringType()),
@@ -23,15 +27,40 @@ SCHEMA = StructType([
     StructField("ville", StringType()),
     StructField("timestamp", StringType()),
 ])
+
+#data frame structurer selon les col et et SCHEMA
+
 df_structured = df_decoded.select(
     from_json(col("value"), SCHEMA).alias("data")
 )
 df_commandes = df_structured.select("data.*")
+df_commandes = df_commandes.withColumn("timestamp",to_timestamp(col("timestamp")))
+#ecrit dans la console avec le bon format les commandes 
 
-query = df_commandes \
-    .writeStream \
-    .outputMode("append") \
-    .format("console") \
+#query = df_commandes \
+#    .writeStream \
+#    .outputMode("append") \
+#    .format("console") \
+#    .start()
+#query.awaitTermination()
+
+
+
+#transferer le data frame dans la table commandes_live dans postgres
+
+
+def ecrire_dans_postgres(batch_df, batch_id):
+    batch_df.write\
+    .mode('append')\
+    .format("jdbc") \
+    .option("url", "jdbc:postgresql://postgres:5432/ecommerce" ) \
+    .option("driver", "org.postgresql.Driver")\
+    .option("dbtable", "silver.commandes_live") \
+    .option("user", os.getenv("POSTGRES_USER")) \
+    .option("password", os.getenv("POSTGRES_PASSWORD")) \
+    .save()    
+
+query = df_commandes.writeStream \
+    .foreachBatch(ecrire_dans_postgres) \
     .start()
-
 query.awaitTermination()
